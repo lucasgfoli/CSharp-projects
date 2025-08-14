67f9528a53852abe88843616bb9fc32bf4d55d21
Lucasgfoli <lucasgabrielfer.oli@gmail.com>
2025-08-14 17:48:14 +0000

    criando projeto 'verificar email

---
+++ verificar-email/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o domínio para verificar (ex: @gmail.com):");
+        string dominio = Console.ReadLine();
+        Console.WriteLine("Digite os emails separados por vírgula:");
+        string[] emails = Console.ReadLine().Split(',');
+
+        VerificarEmail(emails, dominio);
+    }
+    static void VerificarEmail(string[] emails, string dominio)
+    {
+        List<string> emailsVerificados = new List<string>();
+
+        foreach (string email in emails)
+        {
+            string emailTrim = email.Trim();
+
+            if (email.Length > dominio.Length)
+            {
+                if (emailTrim.EndsWith(dominio))
+                    emailsVerificados.Add(emailTrim);
+            }
+        }
+
+        Console.WriteLine("Emails verificados com sucesso:");
+        foreach (string email in emailsVerificados)
+        {
+            Console.WriteLine(email);
+        }
+    }
+}
